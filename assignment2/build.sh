46fe1773cd56149576ef4c17064705e38ad489be
davideCaligola <114524396+davideCaligola@users.noreply.github.com>
2023-01-29 18:34:19 +0100

    removed wrongly added directory

--- assignment2/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ./bin
-gcc ./src/master.c -lncurses -lpthread -o ./bin/master
-gcc ./src/processA.c -lbmp -lm -lncurses -lpthread -o ./bin/processA
-gcc ./src/processB.c -lbmp -lncurses -lm -lpthread -o ./bin/processB
